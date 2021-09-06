cdd5af9073a4e8a0cc33eb6bae0ca4c42f377dd7
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-09-06 18:37:33 +0200

    Add project files

---
+++ routes/seats.routes.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router =express.Router();
+const db = require('../db');
+const { v4: uuidv4 } = require('uuid');
+
+router.route('/seats').get((req, res) => {
+    res.json(db.seats);
+});
+
+router.route('/seats/:id').get((req, res) => {
+    const item = db.seats.find(seat => seat.id === req.params.id);
+    if(item) res.json(item);
+    else res.status(404).json({ message: 'Not found...' });
+});
+
+router.route('/seats').post((req, res) => {
+    const seat ={
+        id: uuidv4(),
+        day: req.body.day,
+        seat: req.body.seat,
+        client: req.body.client,
+        email: req.body.email
+    };
+    db.seats.push(seat);
+    return res.json({
+      message: 'ok'
+    });
+});
+
+router.route('/seats/:id').delete((req, res) => {
+    db.seats.forEach(seat => {
+      if(seat.id && seat.id === req.params.id) {
+        const index = db.seats.indexOf(seat);
+        db.seats.splice(index, 1);
+        return res.json({
+          message: 'ok'
+        });
+      }
+    });
+    res.status(404).json({ message: 'Not found...' })
+});
+
+router.route('/seats/:id').put((req, res) => {
+    db.seats.forEach(seat => {
+      if(seat.id && seat.id === req.params.id) {
+        seat.day = req.body.day;
+        seat.seat = req.body.seat;
+        seat.client = req.body.client;
+        seat.email = req.body.email;
+        return res.json({
+          message: 'ok'
+        });
+      }
+    });
+    res.status(404).json({ message: 'Not found...' });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+
+
+app.use((req, res) => {
+  return res.status(404).json({
+    message: 'Not found...'
+  });
+});
+
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});

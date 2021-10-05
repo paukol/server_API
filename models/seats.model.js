65015469c761051be1f46d7508038502a1b70fe4
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-10-05 23:18:37 +0200

    fix seats.model.js

--- models/seats.model.js
+++ models/seats.model.js
@@ -4,7 +4,7 @@ const seatSchema = new mongoose.Schema({
     seat: { type: Number, required: true },
     client: { type: String, required: true },
     email: { type: String, required: true },
-    concert: { type: String, required: true }
+    day: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Seat', seatSchema);

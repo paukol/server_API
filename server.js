d525b081a33882221ae543551b82125ed412ad74
Paulina Kolonczyk <paulinakolonczyk@interia.pl>
2021-10-14 13:33:32 +0200

    Add config var

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ let dbUri = '';
 
 if(NODE_ENV === 'production') dbUri = 'mongodb://localhost:27017/NewWaveDB';
 else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/NewWaveDBtest';
-else dbUri = 'mongodb+srv://kolonczykpaulina:joj1FFYf5KmSvRy2@cluster0.06oq9.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
+else dbUri = 'mongodb+srv://${process.env.GITHUB_USERNAME}:${process.env.OTHER_VAR}@cluster0.06oq9.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
 
 mongoose.connect(dbUri, {
   useNewUrlParser: true,
